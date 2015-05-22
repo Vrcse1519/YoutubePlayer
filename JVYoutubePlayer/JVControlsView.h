4f4dd28df2441da74a4afcffb3b0b27e28f1639b
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-05-22 16:39:43 -0400

    added overlay view

---
+++ JVYoutubePlayer/JVControlsView.h
@@ -0,0 +1,13 @@
+//
+//  JVControlsView.h
+//  YoutubePlayerDemo
+//
+//  Created by Jorge Valbuena on 2015-05-22.
+//  Copyright (c) 2015 com.jorgedeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVControlsView : UIView
+
+@end
---
+++ JVYoutubePlayer/JVControlsView.m
@@ -0,0 +1,14 @@
+//
+//  JVControlsView.m
+//  YoutubePlayerDemo
+//
+//  Created by Jorge Valbuena on 2015-05-22.
+//  Copyright (c) 2015 com.jorgedeveloper. All rights reserved.
+//
+
+#import "JVControlsView.h"
+
+@implementation JVControlsView
+
+
+@end
